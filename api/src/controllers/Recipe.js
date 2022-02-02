173cf72bee95f06e30744d0284425b477c285113
Agustin Almada <almadaagustin05@gmail.com>
2022-02-02 15:42:00 -0300

    listo db,y parte del back

---
+++ api/src/controllers/Recipe.js
@@ -0,0 +1,53 @@
+const { API_KEY } = process.env;
+const axios = require("axios");
+const { Recipe, DietType } = require("../db");
+const Sequelize = require('sequelize')
+
+const api = async () => {
+  try {
+    const info = await axios.get(
+      `https://api.spoonacular.com/recipes/complexSearch/?apiKey=${API_KEY}&addRecipeInformation=true&number=100`
+    );
+
+    const apiInfo = info.data.results.map((e) => {
+      return {
+        name: e.title,
+        resume: e.summary,
+        score: e.spoonacularScore,
+        healthylevel: e.healthScore,
+        stepByStep: e.analyzedInstructions.map((obj) =>
+          obj.steps.map((obj2) => obj2.step)
+        ),
+        image: e.image,
+        id: e.id,
+        diets: e.diets.map((diet) => diet),
+      };
+    });
+    return apiInfo;
+  } catch (e) {
+      console.log(e)
+  }
+};
+
+const db = async () =>{
+    try {
+        return await Recipe.findAll({
+            includes:{
+                model:DietType,
+                atributes:['name'],
+                throught:{
+                    atributes:[]
+                }
+            }
+        })
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+const allInfo = async () => {
+    const apiInfo = await api();
+    const dbInfo = await db();
+    const allInfo = [...dbInfo,...apiInfo]
+    return allInfo;
+}
